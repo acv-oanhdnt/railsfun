3d198419f86b0d8510431b6f83f76847ef5f8cd2
acv-oanhdnt <oanhdnt@atmarkcafe.org>
2018-08-06 09:20:51 +0000

    add reference between product and category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_06_091110) do
+ActiveRecord::Schema.define(version: 2018_08_06_091755) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 2018_08_06_091110) do
     t.datetime "updated_at", null: false
     t.decimal "price", precision: 6, scale: 2
     t.boolean "published"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_products_on_category_id"
   end
 
 end
