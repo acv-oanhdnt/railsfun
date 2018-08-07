a2131c286b82be549e70e3cba98248a06de4e57b
acv-oanhdnt <oanhdnt@atmarkcafe.org>
2018-08-07 08:25:16 +0000

    add hook for products

--- app/models/product.rb
+++ app/models/product.rb
@@ -12,4 +12,14 @@ class Product < ActiveRecord::Base
    
    scope :published, -> { where(published: true) }
    scope :priced_more_than, ->(price) { where('price > ?', price) }
+   
+   before_save :strip_html_from_description
+   def strip_html_from_description
+      self.description = ActionView::Base.full_sanitizer.sanitize(self.description)
+   end
+   
+   before_save :lower_title
+   def
+      self.title = title.downcase
+   end
 end
