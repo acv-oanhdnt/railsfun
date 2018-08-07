489557ae0b8ed9673c29efdc27825f041cc7241f
acv-oanhdnt <oanhdnt@atmarkcafe.org>
2018-08-07 08:04:09 +0000

    created data in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+ActiveRecord::Base.connection.execute("ALTER SEQUENCE products_id_seq RESTART WITH 1")
+
+Product.create!(title: 'Ruby Book', description: 'Learn Ruby programming', price: 12.99, published: true)
+Product.create!(title: 'jQuery Book', description: 'Learn jQuery', price: 11.99, published: true)
+Product.create!(title: 'SASS Book', description: 'Learn SASS', price: 19.99, published: true)
