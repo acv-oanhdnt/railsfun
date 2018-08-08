9127d408bace7dca398d9b5f83424b3ae4adbe29
acv-oanhdnt <oanhdnt@atmarkcafe.org>
2018-08-08 08:45:32 +0000

    create and new page for product

--- Gemfile
+++ Gemfile
@@ -48,6 +48,13 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  
+  gem 'pry-byebug'
+  gem 'pry-rails'
+  gem 'pry-doc'
+  gem 'pry-stack_explorer'
 end
 
 group :test do
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -10,4 +10,22 @@ class ProductsController < ApplicationController
   def new
     @product = Product.new 
   end
+  
+  def create
+    product_params = params.require(:product).permit(:title,
+                                                      :description,
+                                                      :price,
+                                                      :level,
+                                                      :published,
+                                                      :country,
+                                                      :category_id)
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "You have successfully created the product"
+      redirect_to products_path
+    else
+      flash.now[:notice] = 'There is an error in your form'
+      render :new
+    end
+  end
 end
